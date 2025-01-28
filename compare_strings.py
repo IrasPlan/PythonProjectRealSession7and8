d7f500e03326dbb9bfbf20cc9456e5110737576b
IrasPlan <ignairastorza@gmail.com>
2025-01-28 17:47:27 +0100

    added compared strings

---
+++ compare_strings.py
@@ -0,0 +1,22 @@
+s1 = "banana"
+s2 = "banana"
+print(s1 == s2)
+
+s2 = "Banana"
+print(s1 == s2)
+print(s1 > s2) #bcs capitals make it grater
+
+s1 = "banana"
+s2 = "banany"
+print(s1 > s2)
+
+s1 = "i went to see john"
+s2 = "went"
+print(s2 in s1)
+print("ana" in "banana")
+print("ANA" in "banana")
+
+
+
+
+
