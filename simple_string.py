4829ccacdbdb0eaf9b1902e097b6072b915fbd5f
IrasPlan <ignairastorza@gmail.com>
2025-01-28 16:47:08 +0100

    Initial commit

---
+++ simple_string.py
@@ -0,0 +1,12 @@
+s1 = "John"
+s2 = "Maria"
+
+print(s1)
+print(s2)
+
+s1 = "And then he asked: 'How are you?'"
+print(s1)
+s1 = 'ad then he asked: "how are you?"'
+s1 = "This is how to have \" and \' inside a string" #you can also escape them
+print(s1)
+
